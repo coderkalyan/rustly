c8b4647603d487d38b82b18e5a3d57668fc17f28
Kalyan Sriram <coder.kalyan@gmail.com>
2019-10-11 23:52:36 -0700

    Lay out architecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+#[get("/api/shortcuts/<id>")]
+fn lookup(id: String) -> String {
+    id
+}
+
+#[get("/<id>")]
+fn redirect(id: String) -> String {
+    id
+}
+
+#[post("/api/shortcuts/new/<dest>")]
+fn create(dest: String) -> String {
+    dest
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount(
+            "/",
+            rocket::routes![lookup, redirect, create]
+            )
+        .launch();
 }
